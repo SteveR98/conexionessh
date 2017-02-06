2fae4a07eeea613b5181734627d4e09461f7adaf
alvar_000 <alvarodava@gmail.com>
2017-02-06 15:28:49 +0100

    Conexion ssh

---
+++ conexionssh/Inicio.java
@@ -0,0 +1,13 @@
+package conexionssh;
+
+	public class Inicio {
+
+		public static void main(String[] args) throws Throwable {
+			
+			MainMenu menu = new MainMenu();
+			
+			menu.Iniciomenu();
+		}
+
+	}
+
